69d597cd572f40cb15f30b684167c346d8c82cfb
FC-web <hasanamir663@gmail.com>
2021-06-23 12:28:34 +0100

    final2

--- app1/models.py
+++ app1/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class info(models.Model):
-    ui= models.CharField(max_length=18)
-    password= models.CharField(max_length=18)
+    ui= models.CharField(max_length=32)
+    password= models.CharField(max_length=32)
 
